d6c14acbecd79738baf0ced42ce7d3c44a568457
carlatakagi <carlatakagi@gmail.com>
2022-03-06 11:19:19 -0400

    requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+export const LOGIN_USER = 'LOGIN_USER';
+
+const saveLoginInfo = (email) => ({
+  type: LOGIN_USER,
+  payload: email,
+});
+
+export default saveLoginInfo;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,110 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import './Login.css';
+import PropTypes from 'prop-types';
+import saveLoginInfo from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      isDisabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validateEmailAndPassword = this.validateEmailAndPassword.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    },
+    this.validateEmailAndPassword());
+  }
+
+  // como validar email: https://www.horadecodar.com.br/2020/09/13/como-validar-email-com-javascript/
+  validateEmailAndPassword() {
+    const { email, password } = this.state;
+    const regexEmail = /\S+@\S+\.\S+/;
+    const MIN_LENGTH = 5;
+
+    if (regexEmail.test(email) && password.length >= MIN_LENGTH) {
+      this.setState({
+        isDisabled: false,
+      });
+    } else {
+      this.setState({
+        isDisabled: true,
+      });
+    }
+  }
+
+  /* handleClick(event) {
+    event.preventDefault();
+    // console.log(this.props);
+    const { history } = this.props;
+    const { email } = this.state;
+    saveLoginInfo(email);
+    history.push('/carteira');
+  } */
+
   render() {
-    return <div>Login</div>;
+    const { isDisabled, email, password } = this.state;
+    const { history, dispatch } = this.props;
+
+    return (
+      <div>
+        <h1>Login</h1>
+
+        <form className="login">
+          <label htmlFor="email-input">
+            <input
+              data-testid="email-input"
+              type="email"
+              placeholder="Email"
+              onChange={ this.handleChange }
+              value={ email }
+              name="email"
+            />
+          </label>
+
+          <label htmlFor="password-input">
+            <input
+              data-testid="password-input"
+              type="password"
+              placeholder="Senha"
+              value={ password }
+              name="password"
+              onChange={ this.handleChange }
+            />
+          </label>
+
+          <button
+            type="submit"
+            disabled={ isDisabled }
+            onClick={ (event) => {
+              event.preventDefault();
+              dispatch(saveLoginInfo(email));
+              history.push('/carteira');
+            } }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect()(Login);
